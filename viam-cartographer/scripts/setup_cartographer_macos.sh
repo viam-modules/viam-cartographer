e5a039a83c19dde27543404783f0b3d11629bdc5
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2023-03-01 09:02:32 -0500

    Update setup scripts

--- viam-cartographer/scripts/setup_cartographer_macos.sh
+++ viam-cartographer/scripts/setup_cartographer_macos.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "Installing cartographer external dependencies"
 brew update
 brew upgrade
@@ -6,3 +7,8 @@ brew link lua@5.3
 brew install openssl eigen gflags glog suite-sparse sphinx-doc pcl
 brew link protobuf
 brew link openssl
+echo 'export PKG_CONFIG_PATH="/usr/local/opt/openssl@3/lib/pkgconfig"' >> ~/.viamdevrc
+
+echo -e "\033[0;32m""Dev environment setup is complete!""\033[0m"
+echo -e "Don't forget to restart your shell, or execute: ""\033[41m""source ~/.viamdevrc""\033[0m"
+exit 0
