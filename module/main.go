45f377b0c3a0d8d4694b84a754990d898c567f58
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2023-04-07 15:27:31 -0400

    RSDK-2016 - Add module (#34)
    Co-authored-by: zaporter-work <work-github@zackporter.com>
    Co-authored-by: Nick Sanford <nick.sanford@viam.com>

--- module/main.go
+++ module/main.go
@@ -1,2 +1,39 @@
 // Package main is a module with a cartographer SLAM service model.
 package main
+
+import (
+	"context"
+
+	"github.com/edaniels/golog"
+	"go.viam.com/rdk/module"
+	"go.viam.com/rdk/services/slam"
+	"go.viam.com/utils"
+
+	viamcartographer "github.com/viamrobotics/viam-cartographer"
+)
+
+func main() {
+	utils.ContextualMain(mainWithArgs, golog.NewLogger("cartographerModule"))
+}
+
+func mainWithArgs(ctx context.Context, args []string, logger golog.Logger) error {
+	// Instantiate the module
+	cartoModule, err := module.NewModuleFromArgs(ctx, logger)
+	if err != nil {
+		return err
+	}
+
+	// Add the cartographer model to the module
+	if err = cartoModule.AddModelFromRegistry(ctx, slam.Subtype, viamcartographer.Model); err != nil {
+		return err
+	}
+
+	// Start the module
+	err = cartoModule.Start(ctx)
+	defer cartoModule.Close(ctx)
+	if err != nil {
+		return err
+	}
+	<-ctx.Done()
+	return nil
+}
