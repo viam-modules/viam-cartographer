519e66eb2dd482cf5b515b17b5d0f622be8fac2a
John <john@viam.com>
2023-04-27 17:55:01 -0400

    skip int test

--- integration_test.go
+++ integration_test.go
@@ -383,5 +383,6 @@ func testCartographerDir(t *testing.T, path string, expectedMaps int) {
 }
 
 func TestCartographerIntegration2D(t *testing.T) {
+	t.Skip()
 	integrationtestHelperCartographer(t, slam.Mode(viamcartographer.Dim2d))
 }
