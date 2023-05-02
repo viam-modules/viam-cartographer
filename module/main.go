eda3a881d1a48bcb3fa1fb3418e17428ccfe75c0
Nick Sanford <nick.sanford@viam.com>
2023-05-02 16:36:37 -0400

    add-version-information

--- module/main.go
+++ module/main.go
@@ -12,11 +12,30 @@ import (
 	viamcartographer "github.com/viamrobotics/viam-cartographer"
 )
 
+// Versioning variables which are replaced by LD flags.
+var (
+	Version     = "development"
+	GitRevision = ""
+)
+
+// Arguments for the command.
+type Arguments struct {
+	Version bool `flag:"version,usage=print version"`
+}
+
 func main() {
 	utils.ContextualMain(mainWithArgs, golog.NewLogger("cartographerModule"))
 }
 
 func mainWithArgs(ctx context.Context, args []string, logger golog.Logger) error {
+	argsParsed, err := printVersion(args, logger)
+	if err != nil {
+		return err
+	}
+	if argsParsed.Version {
+		return nil
+	}
+
 	// Instantiate the module
 	cartoModule, err := module.NewModuleFromArgs(ctx, logger)
 	if err != nil {
@@ -36,4 +55,28 @@ func mainWithArgs(ctx context.Context, args []string, logger golog.Logger) error
 	}
 	<-ctx.Done()
 	return nil
+}
+
+func printVersion(args []string, logger golog.Logger) (Arguments, error) {
+	var argsParsed Arguments
+	if err := utils.ParseFlags(args, &argsParsed); err != nil {
+		return argsParsed, err
+	}
+
+	// Always log the version, return early if the '-version' flag was provided
+	// fmt.Println would be better but fails linting. Good enough.
+	var versionFields []interface{}
+	if Version != "" {
+		versionFields = append(versionFields, "version", Version)
+	}
+	if GitRevision != "" {
+		versionFields = append(versionFields, "git_rev", GitRevision)
+	}
+	if len(versionFields) != 0 {
+		logger.Infow(viamcartographer.Model.String(), versionFields...)
+	} else {
+		logger.Info(viamcartographer.Model.String() + " built from source; version unknown")
+	}
+
+	return argsParsed, nil
 }
