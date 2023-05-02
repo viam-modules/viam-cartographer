ae36083e37a560bc1a23e98f512fc3ac8f3e693f
jeremyrhyde <jeremy@viam.com>
2023-05-02 15:58:35 -0400

    update link

--- viam-cartographer/src/slam_service/slam_service.cc
+++ viam-cartographer/src/slam_service/slam_service.cc
@@ -28,8 +28,8 @@ struct ColorARGB {
 };
 
 // Check if the green color channel is 0 to filter unobserved pixels which is set in 
-// DrawTexture at https://github.com/cartographer-project/cartographer/blob/master/
-// cartographer/io/submap_painter.cc#L210-L213
+// DrawTexture at https://github.com/cartographer-project/cartographer/blob/ef00de231
+// 7dcf7895b09f18cc4d87f8b533a019b/cartographer/io/submap_painter.cc#L206-L207
 bool CheckIfEmptyPixel(ColorARGB pixel_color) { return (pixel_color.G == 0); }
 
 // Convert the scale of a specified color channel from the given UCHAR 
