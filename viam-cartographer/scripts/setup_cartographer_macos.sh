4aa97cecef47ce5af30e24bea8734e1242866b5b
Devin Hilly <devin.hilly@viam.com>
2023-05-02 17:08:45 -0400

    No longer need to force link openssl

--- viam-cartographer/scripts/setup_cartographer_macos.sh
+++ viam-cartographer/scripts/setup_cartographer_macos.sh
@@ -6,4 +6,3 @@ brew install abseil boost ceres-solver protobuf ninja cairo googletest lua@5.3
 brew link lua@5.3
 brew install openssl eigen gflags glog suite-sparse sphinx-doc pcl
 brew link protobuf
-brew link openssl --force
