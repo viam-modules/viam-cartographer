638cdc5a22a54e1ae51b3e9164c29632baad720b
Esha Maharishi <esha.maharishi@gmail.com>
2023-05-05 08:11:04 -0400

    Add github workflow for creating rc0 appimages on rc0 tags

--- etc/packaging/appimages/package_release_module.sh
+++ etc/packaging/appimages/package_release_module.sh
@@ -4,9 +4,15 @@ set -e
 SELF=$(realpath $0)
 source "$(dirname $SELF)/utils.sh"
 
-if get_version_tag > /dev/null
+if get_stable_version_tag > /dev/null
 then
-	CUR_TAG=$(get_version_tag)
+	CUR_TAG=$(get_stable_version_tag)
 	BUILD_CHANNEL=stable appimage-builder --recipe cartographer-module-`uname -m`.yml
+	BUILD_CHANNEL=$CUR_TAG appimage-builder --recipe cartographer-module-`uname -m`.yml
+fi
+
+if get_rc_version_tag > /dev/null
+then
+	CUR_TAG=$(get_rc_version_tag)
 	BUILD_CHANNEL=$CUR_TAG appimage-builder --recipe cartographer-module-`uname -m`.yml
 fi
--- etc/packaging/appimages/utils.sh
+++ etc/packaging/appimages/utils.sh
@@ -3,7 +3,7 @@
 # This is a helper function to determine if the current git commit constitutes a new release.
 # Specifically, is it tagged in the format "v1.2.3" and a higher version than any other tags.
 # This is used for internal tagging and release building.
-get_version_tag() {
+get_stable_version_tag() {
 	set -e
 	CUR_TAG=`git tag --points-at | sort -Vr | head -n1`
 	if [[ $CUR_TAG =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]
@@ -16,6 +16,21 @@ get_version_tag() {
 		else
 			echo "latest"
 			return 128
+		fi
+	fi
+	return 1
+}
+
+get_rc_version_tag() {
+	set -e
+	CUR_TAG=`git tag --points-at | sort -Vr | head -n1`
+	if [[ $CUR_TAG =~ ^v[0-9]+\.[0-9]+\.[0-9]+-rc[0-9]+$ ]]
+	then
+		NEWEST_TAG=`git tag -l "v*.*.*" | sort -Vr | head -n1`
+		if [[ $CUR_TAG == $NEWEST_TAG ]]
+		then
+			echo $CUR_TAG
+			return 0
 		fi
 	fi
 	return 1
