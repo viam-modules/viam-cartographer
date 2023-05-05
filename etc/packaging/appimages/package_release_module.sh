e423a6482ffea9fcbabeb327749933992bfbe624
Esha Maharishi <esha.maharishi@viam.com>
2023-05-05 15:07:50 -0400

    Add github workflow for creating rc0 appimages on rc0 tags (#85)

--- etc/packaging/appimages/package_release_module.sh
+++ etc/packaging/appimages/package_release_module.sh
@@ -4,9 +4,13 @@ set -e
 SELF=$(realpath $0)
 source "$(dirname $SELF)/utils.sh"
 
-if get_version_tag > /dev/null
+if get_stable_version_tag > /dev/null
 then
-	CUR_TAG=$(get_version_tag)
-	BUILD_CHANNEL=stable appimage-builder --recipe cartographer-module-`uname -m`.yml
-	BUILD_CHANNEL=$CUR_TAG appimage-builder --recipe cartographer-module-`uname -m`.yml
+	BUILD_CHANNEL="stable" appimage-builder --recipe cartographer-module-`uname -m`.yml
+	BUILD_CHANNEL=$(get_stable_version_tag) appimage-builder --recipe cartographer-module-`uname -m`.yml
+elif get_rc_version_tag > /dev/null
+then
+	BUILD_CHANNEL=$(get_rc_version_tag) appimage-builder --recipe cartographer-module-`uname -m`.yml
+else
+	BUILD_CHANNEL="latest" appimage-builder --recipe cartographer-module-`uname -m`.yml
 fi
