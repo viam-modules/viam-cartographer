49a77817600855b7ba989e1d808e926017a6b79f
Devin Hilly <devin.hilly@viam.com>
2023-05-18 14:30:59 -0400

    Pin to specific openssl version to match build script

--- viam-cartographer/scripts/setup_cartographer_macos.sh
+++ viam-cartographer/scripts/setup_cartographer_macos.sh
@@ -4,5 +4,5 @@ brew update
 brew upgrade
 brew install abseil boost ceres-solver protobuf ninja cairo googletest lua@5.3 pkg-config cmake go@1.20 grpc
 brew link lua@5.3
-brew install openssl eigen gflags glog suite-sparse sphinx-doc pcl
+brew install openssl@3 eigen gflags glog suite-sparse sphinx-doc pcl
 brew link protobuf
