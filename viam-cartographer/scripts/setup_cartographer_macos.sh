5a73892380f0c79bac669cf2ce0aa8202c5ef9b0
JohnN193 <92045055+JohnN193@users.noreply.github.com>
2023-06-08 15:58:48 -0400

    use protobuf@21 for brew setup (#129)

--- viam-cartographer/scripts/setup_cartographer_macos.sh
+++ viam-cartographer/scripts/setup_cartographer_macos.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Installing cartographer external dependencies"
 brew update
-brew install abseil boost ceres-solver protobuf ninja cairo googletest lua@5.3 pkg-config cmake go@1.20 grpc 
+brew install abseil boost ceres-solver protobuf@21 ninja cairo googletest lua@5.3 pkg-config cmake go@1.20 grpc 
 brew link lua@5.3
 brew install openssl@3 eigen gflags glog suite-sparse sphinx-doc pcl
 brew unlink protobuf@3
-brew unlink protobuf && brew link protobuf
+brew unlink protobuf
+brew unlink protobuf@21 && brew link protobuf@21
