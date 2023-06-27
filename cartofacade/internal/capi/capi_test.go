56c5f2d4b194ac1bf00593940d87efd020601ae8
Kim Mishra <kim.mishra@viam.com>
2023-06-27 10:48:09 -0400

    uncommenting tests

--- cartofacade/internal/capi/capi_test.go
+++ cartofacade/internal/capi/capi_test.go
@@ -12,71 +12,71 @@ import (
 	"go.viam.com/utils/artifact"
 )
 
-// func TestGetConfig(t *testing.T) {
-// 	t.Run("config properly converted between C and go", func(t *testing.T) {
-// 		cfg, dir, err := GetTestConfig("mysensor")
-// 		test.That(t, err, test.ShouldBeNil)
-// 		defer os.RemoveAll(dir)
+func TestGetConfig(t *testing.T) {
+	t.Run("config properly converted between C and go", func(t *testing.T) {
+		cfg, dir, err := GetTestConfig("mysensor")
+		test.That(t, err, test.ShouldBeNil)
+		defer os.RemoveAll(dir)
 
-// 		vcc, err := getConfig(cfg)
-// 		test.That(t, err, test.ShouldBeNil)
+		vcc, err := getConfig(cfg)
+		test.That(t, err, test.ShouldBeNil)
 
-// 		sensors := bStringToGoStringSlice(vcc.sensors, int(vcc.sensors_len))
-// 		test.That(t, sensors[0], test.ShouldResemble, "mysensor")
-// 		test.That(t, sensors[1], test.ShouldResemble, "imu")
-// 		test.That(t, vcc.sensors_len, test.ShouldEqual, 2)
+		sensors := bStringToGoStringSlice(vcc.sensors, int(vcc.sensors_len))
+		test.That(t, sensors[0], test.ShouldResemble, "mysensor")
+		test.That(t, sensors[1], test.ShouldResemble, "imu")
+		test.That(t, vcc.sensors_len, test.ShouldEqual, 2)
 
-// 		dataDir := bstringToGoString(vcc.data_dir)
-// 		test.That(t, dataDir, test.ShouldResemble, dir)
+		dataDir := bstringToGoString(vcc.data_dir)
+		test.That(t, dataDir, test.ShouldResemble, dir)
 
-// 		componentReference := bstringToGoString(vcc.component_reference)
-// 		test.That(t, componentReference, test.ShouldResemble, "component")
-// 		freeBstringArray(vcc.sensors, vcc.sensors_len)
+		componentReference := bstringToGoString(vcc.component_reference)
+		test.That(t, componentReference, test.ShouldResemble, "component")
+		freeBstringArray(vcc.sensors, vcc.sensors_len)
 
-// 		test.That(t, vcc.lidar_config, test.ShouldEqual, twoD)
-// 	})
-// }
+		test.That(t, vcc.lidar_config, test.ShouldEqual, twoD)
+	})
+}
 
-// func TestGetPositionResponse(t *testing.T) {
-// 	gpr := getTestGetPositionResponse()
-// 	t.Run("position response properly converted between C and go", func(t *testing.T) {
-// 		holder := toGetPositionResponse(gpr)
-// 		test.That(t, holder.ComponentReference, test.ShouldEqual, "C++ component reference")
+func TestGetPositionResponse(t *testing.T) {
+	gpr := getTestGetPositionResponse()
+	t.Run("position response properly converted between C and go", func(t *testing.T) {
+		holder := toGetPositionResponse(gpr)
+		test.That(t, holder.ComponentReference, test.ShouldEqual, "C++ component reference")
 
-// 		test.That(t, holder.X, test.ShouldEqual, 100)
-// 		test.That(t, holder.Y, test.ShouldEqual, 200)
-// 		test.That(t, holder.Z, test.ShouldEqual, 300)
+		test.That(t, holder.X, test.ShouldEqual, 100)
+		test.That(t, holder.Y, test.ShouldEqual, 200)
+		test.That(t, holder.Z, test.ShouldEqual, 300)
 
-// 		test.That(t, holder.Ox, test.ShouldEqual, 400)
-// 		test.That(t, holder.Oy, test.ShouldEqual, 500)
-// 		test.That(t, holder.Oz, test.ShouldEqual, 600)
+		test.That(t, holder.Ox, test.ShouldEqual, 400)
+		test.That(t, holder.Oy, test.ShouldEqual, 500)
+		test.That(t, holder.Oz, test.ShouldEqual, 600)
 
-// 		test.That(t, holder.Imag, test.ShouldEqual, 700)
-// 		test.That(t, holder.Jmag, test.ShouldEqual, 800)
-// 		test.That(t, holder.Kmag, test.ShouldEqual, 900)
+		test.That(t, holder.Imag, test.ShouldEqual, 700)
+		test.That(t, holder.Jmag, test.ShouldEqual, 800)
+		test.That(t, holder.Kmag, test.ShouldEqual, 900)
 
-// 		test.That(t, holder.Theta, test.ShouldEqual, 1000)
-// 		test.That(t, holder.Real, test.ShouldEqual, 1100)
-// 	})
-// }
+		test.That(t, holder.Theta, test.ShouldEqual, 1000)
+		test.That(t, holder.Real, test.ShouldEqual, 1100)
+	})
+}
 
-// func TestToSensorReading(t *testing.T) {
-// 	t.Run("sensor reading properly converted between c and go", func(t *testing.T) {
-// 		timestamp := time.Date(2021, 8, 15, 14, 30, 45, 100, time.Local)
-// 		sr := toSensorReading("mysensor", []byte("he0llo"), timestamp)
-// 		test.That(t, bstringToGoString(sr.sensor), test.ShouldResemble, "mysensor")
-// 		test.That(t, bstringToGoString(sr.sensor_reading), test.ShouldResemble, "he0llo")
-// 		test.That(t, sr.sensor_reading_time_unix_micro, test.ShouldEqual, timestamp.UnixMicro())
-// 	})
-// }
+func TestToSensorReading(t *testing.T) {
+	t.Run("sensor reading properly converted between c and go", func(t *testing.T) {
+		timestamp := time.Date(2021, 8, 15, 14, 30, 45, 100, time.Local)
+		sr := toSensorReading("mysensor", []byte("he0llo"), timestamp)
+		test.That(t, bstringToGoString(sr.sensor), test.ShouldResemble, "mysensor")
+		test.That(t, bstringToGoString(sr.sensor_reading), test.ShouldResemble, "he0llo")
+		test.That(t, sr.sensor_reading_time_unix_micro, test.ShouldEqual, timestamp.UnixMicro())
+	})
+}
 
-// func TestBstringToByteSlice(t *testing.T) {
-// 	t.Run("b strings are properly converted to byte slices", func(t *testing.T) {
-// 		bstring := goStringToBstring("hell0!")
-// 		bytes := bstringToByteSlice(bstring)
-// 		test.That(t, bytes, test.ShouldResemble, []byte("hell0!"))
-// 	})
-// }
+func TestBstringToByteSlice(t *testing.T) {
+	t.Run("b strings are properly converted to byte slices", func(t *testing.T) {
+		bstring := goStringToBstring("hell0!")
+		bytes := bstringToByteSlice(bstring)
+		test.That(t, bytes, test.ShouldResemble, []byte("hell0!"))
+	})
+}
 
 func TestCGoAPI(t *testing.T) {
 	pvcl, err := NewLib(0, 1)
