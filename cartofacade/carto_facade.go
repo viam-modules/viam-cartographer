e268c7c40a2dd59f62254a12422bf675b0a2b2db
kim-mishra <121991867+kim-mishra@users.noreply.github.com>
2023-06-28 15:25:24 -0400

    RSDK-3828 - create cartofacade wrapper (#172)

--- cartofacade/carto_facade.go
+++ cartofacade/carto_facade.go
@@ -9,6 +9,123 @@ import (
 	"time"
 )
 
+var emptyRequestParams = map[RequestParamType]interface{}{}
+
+// Initialize calls into the cartofacade C code.
+func (cf *CartoFacade) Initialize(ctx context.Context, timeout time.Duration, activeBackgroundWorkers *sync.WaitGroup) error {
+	cf.start(ctx, activeBackgroundWorkers)
+	untyped, err := cf.request(ctx, initialize, emptyRequestParams, timeout)
+	if err != nil {
+		return err
+	}
+
+	carto, ok := untyped.(Carto)
+	if !ok {
+		return errors.New("unable to cast response from cartofacade to a carto struct")
+	}
+
+	cf.carto = &carto
+
+	return nil
+}
+
+// Start calls into the cartofacade C code.
+func (cf *CartoFacade) Start(ctx context.Context, timeout time.Duration) error {
+	_, err := cf.request(ctx, start, emptyRequestParams, timeout)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Stop calls into the cartofacade C code.
+func (cf *CartoFacade) Stop(ctx context.Context, timeout time.Duration) error {
+	_, err := cf.request(ctx, stop, emptyRequestParams, timeout)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Terminate calls into the cartofacade C code.
+func (cf *CartoFacade) Terminate(ctx context.Context, timeout time.Duration) error {
+	_, err := cf.request(ctx, terminate, emptyRequestParams, timeout)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// AddSensorReading calls into the cartofacade C code.
+func (cf *CartoFacade) AddSensorReading(
+	ctx context.Context,
+	timeout time.Duration,
+	sensorName string,
+	currentReading []byte,
+	readingTimestamp time.Time,
+) error {
+	requestParams := map[RequestParamType]interface{}{
+		sensor:    sensorName,
+		reading:   currentReading,
+		timestamp: readingTimestamp,
+	}
+
+	_, err := cf.request(ctx, addSensorReading, requestParams, timeout)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetPosition calls into the cartofacade C code.
+func (cf *CartoFacade) GetPosition(ctx context.Context, timeout time.Duration) (GetPosition, error) {
+	untyped, err := cf.request(ctx, position, emptyRequestParams, timeout)
+	if err != nil {
+		return GetPosition{}, err
+	}
+
+	pos, ok := untyped.(GetPosition)
+	if !ok {
+		return GetPosition{}, errors.New("unable to cast response from cartofacade to a position info struct")
+	}
+
+	return pos, nil
+}
+
+// GetInternalState calls into the cartofacade C code.
+func (cf *CartoFacade) GetInternalState(ctx context.Context, timeout time.Duration) ([]byte, error) {
+	untyped, err := cf.request(ctx, internalState, emptyRequestParams, timeout)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	internalState, ok := untyped.([]byte)
+	if !ok {
+		return []byte{}, errors.New("unable to cast response from cartofacade to a byte slice")
+	}
+
+	return internalState, nil
+}
+
+// GetPointCloudMap calls into the cartofacade C code.
+func (cf *CartoFacade) GetPointCloudMap(ctx context.Context, timeout time.Duration) ([]byte, error) {
+	untyped, err := cf.request(ctx, pointCloudMap, emptyRequestParams, timeout)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	pointCloud, ok := untyped.([]byte)
+	if !ok {
+		return []byte{}, errors.New("unable to cast response from cartofacade to a byte slice")
+	}
+
+	return pointCloud, nil
+}
+
 // RequestType defines the carto C API call that is being made.
 type RequestType int64
 
