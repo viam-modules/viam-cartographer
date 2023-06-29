a8af66609dd85b27833e73f7230bb8981c44a591
Kim Mishra <kim.mishra@viam.com>
2023-06-29 16:17:52 -0400

    WIP tests passing, need to make logs telemetric

--- sensorprocess/sensorprocess.go
+++ sensorprocess/sensorprocess.go
@@ -13,6 +13,19 @@ import (
 	"go.viam.com/rdk/utils/contextutils"
 )
 
+type SensorProcessParams struct {
+	Ctx               context.Context
+	Cartofacade       cartofacade.Interface
+	Lidar             lidar.Lidar
+	PrimarySensorName string
+	DataRateMs        int
+	Timeout           time.Duration
+	Logger            golog.Logger
+
+	timeReq time.Time
+	buf     *bytes.Buffer
+}
+
 // SensorProcess polls the lidar to get the next sensor reading and adds it to the mapBuilder.
 func SensorProcess(
 	ctx context.Context,
@@ -39,14 +52,12 @@ func SensorProcess(
 }
 
 //nolint:unused
-func addSensorReading(
-	ctx context.Context,
-	lidar lidar.Lidar,
-	timeout time.Duration,
-	addSensorReadingReplaySensor func(context.Context, time.Time, *bytes.Buffer, time.Duration) error,
-	addSensorReadingLidar func(context.Context, time.Time, *bytes.Buffer, time.Duration) int,
+func AddSensorReading(
+	params SensorProcessParams,
+	addSensorReadingFromReplaySensor func(SensorProcessParams),
+	addSensorReadingFromLiveReadings func(SensorProcessParams) int,
 ) error {
-	reading, err := lidar.GetData(ctx)
+	reading, err := params.Lidar.GetData(params.Ctx)
 	if err != nil {
 		return err
 	}
@@ -56,70 +67,52 @@ func addSensorReading(
 	if err != nil {
 		return err
 	}
+	params.buf = buf
 
 	timeReq := time.Now()
-	ctx, md := contextutils.ContextWithMetadata(ctx)
+	ctx, md := contextutils.ContextWithMetadata(params.Ctx)
 	timeRequestedMetadata, ok := md[contextutils.TimeRequestedMetadataKey]
+	params.Ctx = ctx
 
 	if ok {
-		timeReq, err := time.Parse(time.RFC3339Nano, timeRequestedMetadata[0])
+		timeReq, err = time.Parse(time.RFC3339Nano, timeRequestedMetadata[0])
 		if err != nil {
 			return err
 		}
-
-		err = addSensorReadingReplaySensor(ctx, timeReq, buf, timeout)
-		if err != nil {
-			return err
-		}
+		params.timeReq = timeReq
+		addSensorReadingFromReplaySensor(params)
 	} else {
-		timeToSleep := addSensorReadingLidar(ctx, timeReq, buf, timeout)
+		params.timeReq = timeReq
+		timeToSleep := addSensorReadingFromLiveReadings(params)
 		time.Sleep(time.Duration(timeToSleep) * time.Millisecond)
 	}
 
 	return nil
 }
 
-func addSensorReadingReplaySensor(
-	ctx context.Context,
-	logger golog.Logger,
-	cartofacade cartofacade.Interface,
-	primarySensorName string,
-	dataRateMs int,
-	timeReq time.Time,
-	buf *bytes.Buffer,
-	timeout time.Duration,
-) {
-	err := cartofacade.AddSensorReading(ctx, timeout, primarySensorName, buf.Bytes(), timeReq)
+func AddSensorReadingFromReplaySensor(params SensorProcessParams) {
+	err := params.Cartofacade.AddSensorReading(params.Ctx, params.Timeout, params.PrimarySensorName, params.buf.Bytes(), params.timeReq)
 	/*
 		while add sensor reading fails, keep trying to add the same reading - in offline mode
 		we want to process each reading so if we cannot acquire the lock we should try again
 	*/
 	for err != nil {
 		select {
-		case <-ctx.Done():
+		case <-params.Ctx.Done():
 			return
 		default:
-			logger.Warnf("Unable to acquire the lock for reading from %v. Trying again", timeReq)
-			err = cartofacade.AddSensorReading(ctx, timeout, primarySensorName, buf.Bytes(), timeReq)
+			params.Logger.Warnf("Unable to acquire the lock for reading from %v. Trying again", params.timeReq)
+			err = params.Cartofacade.AddSensorReading(params.Ctx, params.Timeout, params.PrimarySensorName, params.buf.Bytes(), params.timeReq)
 		}
 	}
 }
 
-func addSensorReadingLidar(
-	ctx context.Context,
-	logger golog.Logger,
-	cartofacade cartofacade.Interface,
-	primarySensorName string,
-	dataRateMs int,
-	timeReq time.Time,
-	buf *bytes.Buffer,
-	timeout time.Duration,
-) int {
+func AddSensorReadingFromLiveReadings(params SensorProcessParams) int {
 	startTime := time.Now()
-	err := cartofacade.AddSensorReading(ctx, timeout, primarySensorName, buf.Bytes(), timeReq)
+	err := params.Cartofacade.AddSensorReading(params.Ctx, params.Timeout, params.PrimarySensorName, params.buf.Bytes(), params.timeReq)
 	if err != nil {
-		logger.Warnf("Unable to acquire the lock. Not processing reading from %v", timeReq)
+		params.Logger.Warnf("Unable to acquire the lock. Not processing reading from %v", params.timeReq)
 	}
 	timeElapsedMs := int(time.Since(startTime).Milliseconds())
-	return int(math.Max(0, float64(dataRateMs-timeElapsedMs)))
+	return int(math.Max(0, float64(params.DataRateMs-timeElapsedMs)))
 }
--- sensorprocess/sensorprocess_test.go
+++ sensorprocess/sensorprocess_test.go
@@ -9,6 +9,9 @@ import (
 
 	"github.com/edaniels/golog"
 	"github.com/viamrobotics/viam-cartographer/cartofacade"
+	"github.com/viamrobotics/viam-cartographer/internal/testhelper"
+	"github.com/viamrobotics/viam-cartographer/sensors/lidar"
+	"go.viam.com/rdk/utils/contextutils"
 	"go.viam.com/test"
 )
 
@@ -26,7 +29,17 @@ func TestAddSensorReadingReplaySensor(t *testing.T) {
 		return nil
 	}
 	// When addSensorReading returns successfully, no infinite loop
-	addSensorReadingReplaySensor(context.Background(), logger, &cf, "good_lidar", 200, time.Now(), buf, 10*time.Second)
+	params := SensorProcessParams{
+		Ctx:               context.Background(),
+		Logger:            logger,
+		Cartofacade:       &cf,
+		PrimarySensorName: "good_lidar",
+		DataRateMs:        200,
+		timeReq:           time.Now(),
+		buf:               buf,
+		Timeout:           10 * time.Second,
+	}
+	AddSensorReadingFromReplaySensor(params)
 
 	cf.AddSensorReadingFunc = func(
 		ctx context.Context,
@@ -41,10 +54,43 @@ func TestAddSensorReadingReplaySensor(t *testing.T) {
 	// When addSensorReading is erroring and the context is cancelled, no infinite loop
 	cancelCtx, cancelFunc := context.WithCancel(context.Background())
 	cancelFunc()
-	addSensorReadingReplaySensor(cancelCtx, logger, &cf, "good_lidar", 200, time.Now(), buf, 10*time.Second)
+
+	params.Ctx = cancelCtx
+	AddSensorReadingFromReplaySensor(params)
+
+	// When addSensorReading fails a few times and then succeeds
+	cancelCtx, cancelFunc = context.WithCancel(context.Background())
+	params.Ctx = cancelCtx
+
+	counter := 0
+	var firstReading []byte
+	var lastReading []byte
+
+	cf.AddSensorReadingFunc = func(
+		ctx context.Context,
+		timeout time.Duration,
+		sensorName string,
+		currentReading []byte,
+		readingTimestamp time.Time,
+	) error {
+		if counter == 0 {
+			firstReading = currentReading
+		}
+		if counter < 4 {
+			counter += 1
+			return errors.New("cant acquire lock")
+		}
+		lastReading = currentReading
+		return nil
+	}
+	AddSensorReadingFromReplaySensor(params)
+	// addSensorReading was called 4 times and the time it succeeded it was called with the same reading it was called with the first time
+	test.That(t, counter, test.ShouldEqual, 4)
+	test.That(t, firstReading, test.ShouldResemble, lastReading)
+	cancelFunc()
 }
 
-func TestAddSensorReadingOnline(t *testing.T) {
+func TestAddSensorReadingLiveReadings(t *testing.T) {
 	logger := golog.NewTestLogger(t)
 	cf := cartofacade.Mock{}
 	buf := bytes.NewBuffer([]byte("12345"))
@@ -60,7 +106,17 @@ func TestAddSensorReadingOnline(t *testing.T) {
 	}
 
 	// When addSensorReading blocks for longer than the data rate
-	timeToSleep := addSensorReadingLidar(context.Background(), logger, &cf, "good_lidar", 200, time.Now(), buf, 10*time.Second)
+	params := SensorProcessParams{
+		Ctx:               context.Background(),
+		Logger:            logger,
+		Cartofacade:       &cf,
+		PrimarySensorName: "good_lidar",
+		DataRateMs:        200,
+		timeReq:           time.Now(),
+		buf:               buf,
+		Timeout:           10 * time.Second,
+	}
+	timeToSleep := AddSensorReadingFromLiveReadings(params)
 	test.That(t, timeToSleep, test.ShouldEqual, 0)
 
 	cf.AddSensorReadingFunc = func(
@@ -72,8 +128,67 @@ func TestAddSensorReadingOnline(t *testing.T) {
 	) error {
 		return nil
 	}
+	params.Cartofacade = &cf
 
 	// When addSensorReading does not block for longer than the data rate
-	timeToSleep = addSensorReadingLidar(context.Background(), logger, &cf, "good_lidar", 200, time.Now(), buf, 10*time.Second)
-	test.That(t, timeToSleep, test.ShouldNotEqual, 0)
+	dataRateMs := 200
+	timeToSleep = AddSensorReadingFromLiveReadings(params)
+	test.That(t, timeToSleep, test.ShouldBeGreaterThan, 0)
+	test.That(t, timeToSleep, test.ShouldBeLessThanOrEqualTo, dataRateMs)
+}
+
+func TestAddSensorReading(t *testing.T) {
+	logger := golog.NewTestLogger(t)
+
+	sensors := []string{"good_lidar"}
+	sensor, err := lidar.New(testhelper.SetupDeps(sensors), sensors, 0)
+	test.That(t, err, test.ShouldBeNil)
+
+	addSensorReadingFromReplaySensor := func(params SensorProcessParams) {
+		return
+	}
+	addSensorReadingLiveReadings := func(params SensorProcessParams) int {
+		return 100
+	}
+	cf := cartofacade.Mock{}
+
+	params := SensorProcessParams{
+		Ctx:               context.Background(),
+		Logger:            logger,
+		Cartofacade:       &cf,
+		PrimarySensorName: "replay_sensor",
+		DataRateMs:        200,
+		timeReq:           time.Now(),
+		Lidar:             sensor,
+		Timeout:           10 * time.Second,
+	}
+
+	// Live lidar
+	err = AddSensorReading(params, addSensorReadingFromReplaySensor, addSensorReadingLiveReadings)
+	test.That(t, err, test.ShouldBeNil)
+
+	// Replay sensor with valid time
+	sensors = []string{"replay_sensor"}
+	replaySensor, err := lidar.New(testhelper.SetupDeps(sensors), sensors, 0)
+	test.That(t, err, test.ShouldBeNil)
+
+	ctx, _ := contextutils.ContextWithMetadata(context.Background())
+	params.Ctx = ctx
+	params.Lidar = replaySensor
+	params.PrimarySensorName = "replay_sensor"
+	err = AddSensorReading(params, addSensorReadingFromReplaySensor, addSensorReadingLiveReadings)
+	test.That(t, err, test.ShouldBeNil)
+
+	// Replay sensor with invalid time
+	sensors = []string{"invalid_replay_sensor"}
+	replaySensor, err = lidar.New(testhelper.SetupDeps(sensors), sensors, 0)
+	test.That(t, err, test.ShouldBeNil)
+
+	params.Ctx = ctx
+	params.Lidar = replaySensor
+	params.PrimarySensorName = "invalid_replay_sensor"
+	ctx, _ = contextutils.ContextWithMetadata(context.Background())
+	err = AddSensorReading(params, addSensorReadingFromReplaySensor, addSensorReadingLiveReadings)
+	test.That(t, err, test.ShouldBeError)
+
 }
