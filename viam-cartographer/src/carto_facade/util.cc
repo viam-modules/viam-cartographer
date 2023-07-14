88569c30d620d8b76558e94cda1041af311ca88a
Nick Sanford <nick.sanford@viam.com>
2023-07-14 16:36:03 -0400

    fix-error-log

--- viam-cartographer/src/carto_facade/util.cc
+++ viam-cartographer/src/carto_facade/util.cc
@@ -177,8 +177,8 @@ carto_sensor_reading(std::string sensor_reading,
         return {false, point_cloud};
     }
     VLOG(1) << "read_pcd succeeded";
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(
-        new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud(
+        new pcl::PointCloud<pcl::PointXYZRGBA>);
     pcl::fromPCLPointCloud2(blob, *cloud);
 
     VLOG(1) << "Loaded " << cloud->width * cloud->height << " data points";
