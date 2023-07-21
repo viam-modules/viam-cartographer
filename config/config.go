09c0bdaa432a3e78c05d28575a68d14a8bf19b21
PStrutz <patricia@4strutz.de>
2023-07-21 12:03:45 -0400

    go config lidar rename

--- config/config.go
+++ config/config.go
@@ -14,19 +14,19 @@ func newError(configError string) error {
 
 // Config describes how to configure the SLAM service.
 type Config struct {
-	Sensors       []string          `json:"sensors"`
+	Camera        map[string]string `json:"camera"`
 	ConfigParams  map[string]string `json:"config_params"`
 	DataDirectory string            `json:"data_dir"`
-	DataRateMsec  int               `json:"data_rate_msec"`
+	DataFreqHz    int               `json:"data_freq_hz"`
 	MapRateSec    *int              `json:"map_rate_sec"`
 }
 
-var errSensorsMustNotBeEmpty = errors.New("\"sensors\" must not be empty")
+var errCameraMustNotBeEmpty = errors.New("\"camera\" must not be empty")
 
 // Validate creates the list of implicit dependencies.
-func (config *Config) Validate(path string) ([]string, error) {
-	if config.Sensors == nil || len(config.Sensors) < 1 {
-		return nil, utils.NewConfigValidationError(path, errSensorsMustNotBeEmpty)
+func (config *Config) Validate(path string) (map[string]string, error) {
+	if config.Camera == nil || len(config.Camera) < 1 {
+		return nil, utils.NewConfigValidationError(path, errCameraMustNotBeEmpty)
 	}
 
 	if config.ConfigParams["mode"] == "" {
@@ -37,15 +37,15 @@ func (config *Config) Validate(path string) ([]string, error) {
 		return nil, utils.NewConfigValidationFieldRequiredError(path, "data_dir")
 	}
 
-	if config.DataRateMsec < 0 {
-		return nil, errors.New("cannot specify data_rate_msec less than zero")
+	if config.DataFreqHz < 0 {
+		return nil, errors.New("cannot specify data_freq_hz less than zero")
 	}
 
 	if config.MapRateSec != nil && *config.MapRateSec < 0 {
 		return nil, errors.New("cannot specify map_rate_sec less than zero")
 	}
 
-	deps := config.Sensors
+	deps := config.Camera
 
 	return deps, nil
 }
@@ -53,12 +53,12 @@ func (config *Config) Validate(path string) ([]string, error) {
 // GetOptionalParameters sets any unset optional config parameters to the values passed to this function,
 // and returns them.
 func GetOptionalParameters(config *Config,
-	defaultDataRateMsec, defaultMapRateSec int, logger golog.Logger,
+	defaultDataFreqHz, defaultMapRateSec int, logger golog.Logger,
 ) (int, int) {
-	dataRateMsec := config.DataRateMsec
-	if config.DataRateMsec == 0 {
-		dataRateMsec = defaultDataRateMsec
-		logger.Debugf("no data_rate_msec given, setting to default value of %d", defaultDataRateMsec)
+	dataFreqHz := config.DataFreqHz
+	if config.DataFreqHz == 0 {
+		dataFreqHz = defaultDataFreqHz
+		logger.Debugf("no data_freq_hz given, setting to default value of %d", defaultDataFreqHz)
 	}
 
 	mapRateSec := 0
@@ -69,5 +69,5 @@ func GetOptionalParameters(config *Config,
 		mapRateSec = *config.MapRateSec
 	}
 
-	return dataRateMsec, mapRateSec
+	return dataFreqHz, mapRateSec
 }
