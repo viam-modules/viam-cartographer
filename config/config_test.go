b031648e3df775d050fdb29d15ae4a0fe9be5366
Kim Mishra <kim.mishra@viam.com>
2023-08-10 13:06:35 -0400

    refactor tests so we can test all combos of ffs

--- config/config_test.go
+++ config/config_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/edaniels/golog"
+	"go.uber.org/zap"
 	"go.viam.com/rdk/resource"
 	"go.viam.com/rdk/services/slam"
 	"go.viam.com/test"
@@ -198,11 +199,16 @@ func makeCfgService(IMUIntegrationEnabled, cloudStoryEnabled bool) resource.Conf
 	return cfgService
 }
 
-func TestGetOptionalParameters(t *testing.T) {
+func getOptionalParametersTestHelper(
+	t *testing.T,
+	imuIntegrationEnabled bool,
+	cloudStoryEnabled bool,
+	suffix string,
+) {
 	logger := golog.NewTestLogger(t)
 
-	t.Run("Pass default parameters", func(t *testing.T) {
-		cfgService := makeCfgService(false, false)
+	t.Run(fmt.Sprintf("Pass default parameters%s", suffix), func(t *testing.T) {
+		cfgService := makeCfgService(imuIntegrationEnabled, cloudStoryEnabled)
 		cfgService.Attributes["sensors"] = []string{"a"}
 		cfg, err := newConfig(cfgService)
 		test.That(t, err, test.ShouldBeNil)
@@ -218,81 +224,25 @@ func TestGetOptionalParameters(t *testing.T) {
 		test.That(t, enableMapping, test.ShouldBeFalse)
 	})
 
-	t.Run("Return overrides", func(t *testing.T) {
-		cfgService := makeCfgService(false, false)
-		cfgService.Attributes["camera"] = map[string]string{
-			"name":              "a",
-			"data_frequency_hz": "1",
+	t.Run(fmt.Sprintf("Return overrides%s", suffix), func(t *testing.T) {
+		cfgService := makeCfgService(imuIntegrationEnabled, cloudStoryEnabled)
+		if imuIntegrationEnabled {
+			cfgService.Attributes["camera"] = map[string]string{
+				"name":              "testNameCamera",
+				"data_frequency_hz": "2",
+			}
+			cfgService.Attributes["movement_sensor"] = map[string]string{
+				"name":              "testNameSensor",
+				"data_frequency_hz": "2",
+			}
 		}
+
 		cfg, err := newConfig(cfgService)
 		two := 2
 		cfg.MapRateSec = &two
 		dataRate := 50
 		cfg.DataRateMsec = &dataRate
 		test.That(t, err, test.ShouldBeNil)
-		lidarDataRateMsec, _, _, mapRateSec, enableMapping, err := GetOptionalParameters(
-			cfg,
-			1000,
-			1000,
-			1002,
-			logger)
-		test.That(t, err, test.ShouldBeNil)
-		test.That(t, mapRateSec, test.ShouldEqual, 2)
-		test.That(t, lidarDataRateMsec, test.ShouldEqual, 50)
-		test.That(t, enableMapping, test.ShouldBeFalse)
-	})
-}
-
-func newConfig(conf resource.Config) (*Config, error) {
-	slamConf, err := resource.TransformAttributeMap[*Config](conf.Attributes)
-	if err != nil {
-		return &Config{}, newError(err.Error())
-	}
-
-	if _, err := slamConf.Validate("services.slam.attributes.fake"); err != nil {
-		return &Config{}, newError(err.Error())
-	}
-
-	return slamConf, nil
-}
-
-func TestGetOptionalParametersFeatureFlag(t *testing.T) {
-	logger := golog.NewTestLogger(t)
-
-	t.Run("Pass default parameters with feature flag enabled", func(t *testing.T) {
-		cfgService := makeCfgService(true, false)
-		cfgService.Attributes["camera"] = map[string]string{"name": "a"}
-		cfgService.Attributes["movement_sensor"] = map[string]string{"name": "b"}
-		cfg, err := newConfig(cfgService)
-		test.That(t, err, test.ShouldBeNil)
-		lidarDataRateMsec, imuName, imuDataRateMsec, mapRateSec, enableMapping, err := GetOptionalParameters(
-			cfg,
-			1000,
-			1000,
-			1002,
-			logger)
-		test.That(t, err, test.ShouldBeNil)
-		test.That(t, mapRateSec, test.ShouldEqual, 1002)
-		test.That(t, lidarDataRateMsec, test.ShouldEqual, 1000)
-		test.That(t, imuName, test.ShouldEqual, "b")
-		test.That(t, imuDataRateMsec, test.ShouldEqual, 1000)
-		test.That(t, enableMapping, test.ShouldBeFalse)
-	})
-
-	t.Run("Return overrides with feature flag enabled", func(t *testing.T) {
-		cfgService := makeCfgService(true, false)
-		cfgService.Attributes["camera"] = map[string]string{
-			"name":              "a",
-			"data_frequency_hz": "5",
-		}
-		cfgService.Attributes["movement_sensor"] = map[string]string{
-			"name":              "b",
-			"data_frequency_hz": "20",
-		}
-		cfg, err := newConfig(cfgService)
-		two := 2
-		cfg.MapRateSec = &two
-		test.That(t, err, test.ShouldBeNil)
 		lidarDataRateMsec, imuName, imuDataRateMsec, mapRateSec, enableMapping, err := GetOptionalParameters(
 			cfg,
 			1000,
@@ -301,14 +251,29 @@ func TestGetOptionalParametersFeatureFlag(t *testing.T) {
 			logger)
 		test.That(t, err, test.ShouldBeNil)
 		test.That(t, mapRateSec, test.ShouldEqual, 2)
-		test.That(t, lidarDataRateMsec, test.ShouldEqual, 200)
-		test.That(t, imuName, test.ShouldEqual, "b")
-		test.That(t, imuDataRateMsec, test.ShouldEqual, 50)
+		if imuIntegrationEnabled {
+			test.That(t, imuName, test.ShouldEqual, "testNameSensor")
+			test.That(t, imuDataRateMsec, test.ShouldEqual, 500)
+			test.That(t, lidarDataRateMsec, test.ShouldEqual, 500)
+		} else {
+			test.That(t, lidarDataRateMsec, test.ShouldEqual, 50)
+		}
 		test.That(t, enableMapping, test.ShouldBeFalse)
 	})
 
+	if imuIntegrationEnabled {
+		sensorAttributeTestHelper(t, logger, imuIntegrationEnabled, cloudStoryEnabled)
+	}
+}
+
+func sensorAttributeTestHelper(
+	t *testing.T,
+	logger *zap.SugaredLogger,
+	imuIntegrationEnabled bool,
+	cloudStoryEnabled bool,
+) {
 	t.Run("Unit test return error if lidar data frequency is invalid", func(t *testing.T) {
-		cfgService := makeCfgService(true, false)
+		cfgService := makeCfgService(imuIntegrationEnabled, cloudStoryEnabled)
 		cfgService.Attributes["camera"] = map[string]string{
 			"name":              "a",
 			"data_frequency_hz": "b",
@@ -325,7 +290,7 @@ func TestGetOptionalParametersFeatureFlag(t *testing.T) {
 	})
 
 	t.Run("Unit test return error if imu data frequency is invalid", func(t *testing.T) {
-		cfgService := makeCfgService(true, false)
+		cfgService := makeCfgService(imuIntegrationEnabled, cloudStoryEnabled)
 		cfgService.Attributes["camera"] = map[string]string{
 			"name":              "a",
 			"data_frequency_hz": "1",
@@ -344,6 +309,26 @@ func TestGetOptionalParametersFeatureFlag(t *testing.T) {
 			logger)
 		test.That(t, err, test.ShouldBeError, newError("movement_sensor[data_frequency_hz] must only contain digits"))
 	})
+}
+
+func TestGetOptionalParameters(t *testing.T) {
+	getOptionalParametersTestHelper(t, false, false, " with imuIntegrationEnabled = false & cloudStoryEnabled = false")
+	getOptionalParametersTestHelper(t, false, true, " with imuIntegrationEnabled = false & cloudStoryEnabled = true")
+	getOptionalParametersTestHelper(t, true, false, " with imuIntegrationEnabled = true & cloudStoryEnabled = false")
+	getOptionalParametersTestHelper(t, true, true, " with imuIntegrationEnabled = true & cloudStoryEnabled = true")
+}
+
+func newConfig(conf resource.Config) (*Config, error) {
+	slamConf, err := resource.TransformAttributeMap[*Config](conf.Attributes)
+	if err != nil {
+		return &Config{}, newError(err.Error())
+	}
+
+	if _, err := slamConf.Validate("services.slam.attributes.fake"); err != nil {
+		return &Config{}, newError(err.Error())
+	}
+
+	return slamConf, nil
 }
 
 func newConfigWithoutValidate(conf resource.Config) (*Config, error) {
